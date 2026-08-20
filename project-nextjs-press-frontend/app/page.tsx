f7593c5cbfd0c5c6e5a32feb8840d74598ead889
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-08-21 05:52:50 +0600

    27-7 Linking and Navigation in Next js

--- project-nextjs-press-frontend/app/page.tsx
+++ project-nextjs-press-frontend/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home Page
+      <p>
+        Blog Page: <Link href={"/blogs/1"}>Blogs</Link>
+      </p>
+    </div>
+  );
 }
