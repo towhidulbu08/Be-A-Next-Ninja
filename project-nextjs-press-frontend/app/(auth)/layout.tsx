5ced3ef5e74c15803e26e31078f37346c16f05ba
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-08-21 12:33:14 +0600

    28-1 Route Grouping In NextJS

---
+++ project-nextjs-press-frontend/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      AuthLayout
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
---
+++ project-nextjs-press-frontend/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div>LoginbPage</div>;
+};
+
+export default LoginPage;
