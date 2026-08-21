e4c55fb5544f5bcdf29e342e4943a851ae5b788b
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-08-21 09:23:43 +0600

    27-9 App Router Vs Pages Router In NextJS.mp4

--- project-nextjs-press-frontend/app/blogs/page.tsx
+++ project-nextjs-press-frontend/app/blogs/page.tsx
@@ -1,4 +1,12 @@
-const BlogsPage = () => {
+const BlogsPage = async () => {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: {
+      revalidate: 60,
+    },
+  });
+  const data = await posts.json();
+
+  console.log("posts", data);
   return <div>Blogs Page</div>;
 };
 
