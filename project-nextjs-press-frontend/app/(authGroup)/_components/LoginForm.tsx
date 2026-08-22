4d1ca7e523c313d72df081ef6a7b9e9bbc38767c
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-08-22 19:23:06 +0600

    29-1 Creating Login Form With ShadCn

---
+++ project-nextjs-press-frontend/app/(authGroup)/_components/LoginForm.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+const LoginForm = () => {
+  return (
+    <form className="space-y-4">
+      <Card className="p-5 space-y-4">
+        <Input
+          name="email"
+          type="email"
+          placeholder="Enter Your Email"
+          required
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Enter Your Password"
+          required
+        />
+        <Button type="submit">Login</Button>
+      </Card>
+    </form>
+  );
+};
+
+export default LoginForm;
--- project-nextjs-press-frontend/app/(authGroup)/login/page.tsx
+++ project-nextjs-press-frontend/app/(authGroup)/login/page.tsx
@@ -1,66 +1,22 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import LoginBtn from "../_components/LoginBtn";
+import LoginForm from "../_components/LoginForm";
 
 export default function LoginPage() {
-  console.log(process.env.BACKEND_API_URL);
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign Up</Button>
-        </CardAction>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <a
-                  href="#"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
+    <>
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="w-full max-w-md space-y-6 rounded-lg border p-8 shadow-lg">
+          {/* FORM GENERIC TEXTS */}
+          <div className="space-y-2 text-center">
+            <h1 className="text-3xl font-bold">Welcome Back!</h1>
+            <p className="text-gray-500">
+              Enter your credentials to access your account
+            </p>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
-        <LoginBtn />
-      </CardFooter>
-    </Card>
+
+          {/* FORM */}
+          <LoginForm />
+        </div>
+      </div>
+    </>
   );
 }
