11f33146bc9dc4034124f08db4f49928868fcd26
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-08-28 19:20:49 +0600

    30-6 Authenticating Protected Routes In Proxy

--- project-nextjs-press-frontend/proxy.ts
+++ project-nextjs-press-frontend/proxy.ts
@@ -4,6 +4,8 @@ import { NextResponse } from "next/server";
 
 const Auth_Routes = ["/login", "/register"];
 
+const PUBLIC_ROUTES = ["/", "/news"];
+
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
@@ -25,17 +27,26 @@ export async function proxy(request: NextRequest) {
   if (decodedToken && Auth_Routes.includes(pathname)) {
     if (userRole === "USER") {
       return NextResponse.redirect(new URL("/dashboard", request.url));
-    } 
-    else if (userRole==="ADMIN") {
-       return NextResponse.redirect(new URL("/admin-dashboard", request.url));
-    }
-    else if (userRole==="AUTHOR") {
-       return NextResponse.redirect(new URL("/author-dashboard", request.url));
-    }
-    else{
-      return NextResponse.redirect(new URL('/',request.url))
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else if (userRole === "AUTHOR") {
+      return NextResponse.redirect(new URL("/author-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
     }
   }
+
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  const isAuthRoute = Auth_Routes.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  // Authenticated Pages Protection:Authorization is not handled yet
+  if (!accessToken && !isPublicRoute && !isAuthRoute) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
   return NextResponse.next();
 }
 
