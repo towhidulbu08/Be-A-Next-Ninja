6b1d8c80fbaa0319e61fc6a265c4055820e01665
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-09-06 22:15:09 +0600

    32-2 Implementing Functionalities Of Create News / Post Form Modal / Dialog With Cache Invalidation

--- project-nextjs-press-frontend/app/(dashboardGroup)/_actions/myPostsActions.ts
+++ project-nextjs-press-frontend/app/(dashboardGroup)/_actions/myPostsActions.ts
@@ -1,4 +1,58 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+
+import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
+
+type PostState = {
+  success: true;
+  statusCode: number;
+  message: string;
+  data: Record<string, any>;
+};
+
+export const createPost = async (prevState: PostState, formData: FormData) => {
+  const payload = {
+    title: formData.get("title"),
+    content: formData.get("content"),
+    thumbnail: formData.get("thumbnail"),
+    tags: (formData.get("tags") as string).split(", "),
+    isPremium: formData.get("isPremium"),
+  };
+  const cookiesStore = await cookies();
+
+  const accessToken = cookiesStore.get("accessToken");
+
+  if (!accessToken) {
+    return {
+      success: false,
+      message: "User Not Logged In",
+    };
+  }
+  const res = await fetch(`${process.env.BACKEND_API_URL}/api/posts`, {
+    method: "POST",
+    headers: {
+      // Authorization: accessToken?.value as unknown as string,
+      // Authorization: `${accessToken.value}`,
+      // Authorization: `Bearer ${accessToken.value}`,
+      Cookie: `accessToken=${accessToken.value}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  const result = await res.json();
+
+  if (result.success) {
+    revalidateTag("my-profile", "max");
+  }
+  if (result.success && result.data.isPremium) {
+    revalidateTag("premium-posts", "max");
+  } else {
+    revalidateTag("public-news", "max");
+  }
+  return result;
+};
 
 export const getMyPosts = async () => {
   const cookiesStore = await cookies();
--- project-nextjs-press-frontend/app/(dashboardGroup)/_components/PostFormDialog.tsx
+++ project-nextjs-press-frontend/app/(dashboardGroup)/_components/PostFormDialog.tsx
@@ -18,6 +18,7 @@ import { IPost } from "@/lib/types";
 import { PencilIcon, PlusIcon } from "lucide-react";
 import { useActionState, useEffect, useState } from "react";
 import { toast } from "sonner";
+import { createPost } from "../_actions/myPostsActions";
 
 type PostFormDialogProps = {
   mode: "create" | "edit";
@@ -27,7 +28,7 @@ type PostFormDialogProps = {
 export default function PostFormDialog({ mode, post }: PostFormDialogProps) {
   const [open, setOpen] = useState(false);
 
-  const action = mode === "edit" && post ? () => {} : () => {};
+  const action = mode === "edit" && post ? () => {} : createPost;
 
   const [state, formAction, pending] = useActionState(action, null) as any;
 
