9f65e57bf8bba543b3f0a1ff45047e3c5877f6a8
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-09-07 13:09:56 +0600

    32-4 Designing Sidebar For Dashboard Routes

--- project-nextjs-press-frontend/app/(dashboardGroup)/layout.tsx
+++ project-nextjs-press-frontend/app/(dashboardGroup)/layout.tsx
@@ -1,13 +1,19 @@
 import { Navbar } from "@/components/shared/navbar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { getMe } from "@/service/getMe";
-import React from "react";
+import DashboardSidebar from "./_components/DashBoardSideBar";
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const user = await getMe();
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbar user={user} />
-      {children}`
+      <SidebarProvider>
+        <div className="flex flex-1">
+          <DashboardSidebar user={user} />
+          <main className="flex-1 min-w-0">{children}</main>
+        </div>
+      </SidebarProvider>
     </div>
   );
 };
